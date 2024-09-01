dac8f36d8d3ce65b39f3220cb7b98a12d6f1f406
REIJI007 <miyajimareji007@gmail.com>
2024-09-01 20:35:36 +0800

    Update main.go
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- main.go
+++ main.go
@@ -1,66 +1,116 @@
 package main
 
 import (
-	"bufio"
+	"encoding/base64"
 	"flag"
 	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
 
-	"github.com/golang/protobuf/proto"
-	"github.com/your/repo/protobuf/domainpb" // 假设你有一个用于序列化的 Protobuf package
+	"google.golang.org/protobuf/proto"
 )
 
 var (
-	inputFile   = flag.String("inputfile", filepath.Join("./", "adblock.txt"), "Path to the adblock.txt file")
-	outputFile  = flag.String("outputfile", filepath.Join("./", "adblock.dat"), "Path to the generated adblock.dat file")
+	datName      = flag.String("datname", "adblock.dat", "Name of the generated dat file")
+	outputPath   = flag.String("outputpath", "./publish", "Output path to the generated files")
+	exportLists  = flag.String("exportlists", "", "Lists to be exported in plaintext format, separated by ',' comma")
+	excludeAttrs = flag.String("excludeattrs", "", "Exclude rules with certain attributes in certain lists, separated by ',' comma")
+	toGFWList    = flag.String("togfwlist", "", "List to be exported in GFWList format")
 )
 
 func main() {
 	flag.Parse()
 
-	// 读取 adblock.txt 文件
-	file, err := os.Open(*inputFile)
-	if err != nil {
-		fmt.Println("Failed to open input file:", err)
+	adblockFile := "./adblock.txt"
+	listInfoMap := make(ListInfoMap)
+
+	if err := listInfoMap.Marshal(adblockFile); err != nil {
+		fmt.Println("Failed:", err)
 		os.Exit(1)
 	}
-	defer file.Close()
 
-	// 初始化 DomainList
-	var domainList domainpb.DomainList
+	if err := listInfoMap.FlattenAndGenUniqueDomainList(); err != nil {
+		fmt.Println("Failed:", err)
+		os.Exit(1)
+	}
 
-	scanner := bufio.NewScanner(file)
-	for scanner.Scan() {
-		domain := strings.TrimSpace(scanner.Text())
-		if domain != "" && !strings.HasPrefix(domain, "#") {
-			// 创建新的 Domain 对象并设置类型和类别
-			d := &domainpb.Domain{
-				Type: domainpb.Domain_ROUTER,
-				Value: domain,
-				Category: "adblock",
+	excludeAttrsInFile := make(map[fileName]map[attribute]bool)
+	if *excludeAttrs != "" {
+		exFilenameAttrSlice := strings.Split(*excludeAttrs, ",")
+		for _, exFilenameAttr := range exFilenameAttrSlice {
+			exFilenameAttr = strings.TrimSpace(exFilenameAttr)
+			exFilenameAttrMap := strings.Split(exFilenameAttr, "@")
+			filename := fileName(strings.ToUpper(strings.TrimSpace(exFilenameAttrMap[0])))
+			excludeAttrsInFile[filename] = make(map[attribute]bool)
+			for _, attr := range exFilenameAttrMap[1:] {
+				attr = strings.TrimSpace(attr)
+				if len(attr) > 0 {
+					excludeAttrsInFile[filename][attribute(attr)] = true
+				}
 			}
-			domainList.Domains = append(domainList.Domains, d)
 		}
 	}
 
-	if err := scanner.Err(); err != nil {
-		fmt.Println("Failed to scan input file:", err)
+	var exportListsSlice []string
+	if *exportLists != "" {
+		tempSlice := strings.Split(*exportLists, ",")
+		for _, exportList := range tempSlice {
+			exportList = strings.TrimSpace(exportList)
+			if len(exportList) > 0 {
+				exportListsSlice = append(exportListsSlice, exportList)
+			}
+		}
+	}
+
+	if geositeList := listInfoMap.ToProto(excludeAttrsInFile); geositeList != nil {
+		protoBytes, err := proto.Marshal(geositeList)
+		if err != nil {
+			fmt.Println("Failed:", err)
+			os.Exit(1)
+		}
+		if err := os.MkdirAll(*outputPath, 0755); err != nil {
+			fmt.Println("Failed:", err)
+			os.Exit(1)
+		}
+		if err := os.WriteFile(filepath.Join(*outputPath, *datName), protoBytes, 0644); err != nil {
+			fmt.Println("Failed:", err)
+			os.Exit(1)
+		} else {
+			fmt.Printf("%s has been generated successfully in '%s'.\n", *datName, *outputPath)
+		}
+	}
+
+	if filePlainTextBytesMap, err := listInfoMap.ToPlainText(exportListsSlice); err == nil {
+		for filename, plaintextBytes := range filePlainTextBytesMap {
+			filename += ".txt"
+			if err := os.WriteFile(filepath.Join(*outputPath, filename), plaintextBytes, 0644); err != nil {
+				fmt.Println("Failed:", err)
+				os.Exit(1)
+			} else {
+				fmt.Printf("%s has been generated successfully in '%s'.\n", filename, *outputPath)
+			}
+		}
+	} else {
+		fmt.Println("Failed:", err)
 		os.Exit(1)
 	}
 
-	// 序列化到 .dat 文件
-	protoBytes, err := proto.Marshal(&domainList)
-	if err != nil {
-		fmt.Println("Failed to marshal domain list:", err)
+	if gfwlistBytes, err := listInfoMap.ToGFWList(*toGFWList); err == nil {
+		if f, err := os.OpenFile(filepath.Join(*outputPath, "gfwlist.txt"), os.O_RDWR|os.O_CREATE, 0644); err != nil {
+			fmt.Println("Failed:", err)
+			os.Exit(1)
+		} else {
+			encoder := base64.NewEncoder(base64.StdEncoding, f)
+			defer encoder.Close()
+			if _, err := encoder.Write(gfwlistBytes); err != nil {
+				fmt.Println("Failed:", err)
+				os.Exit(1)
+			}
+			fmt.Printf("gfwlist.txt has been generated successfully in '%s'.\n", *outputPath)
+		}
+	} else {
+		fmt.Println("Failed:", err)
 		os.Exit(1)
 	}
-
-	if err := os.WriteFile(*outputFile, protoBytes, 0644); err != nil {
-		fmt.Println("Failed to write output file:", err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("Successfully generated %s.\n", *outputFile)
 }
