14754b04b8a4bd7ef0755ecd625cd3ac780d5c1f
REIJI007 <miyajimareji007@gmail.com>
2024-09-03 20:04:58 +0800

    Update adblock_rule_generator.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator.ps1
+++ adblock_rule_generator.ps1
@@ -1,8 +1,8 @@
 # Title: AdBlock_Rule_For_V2ray
 # Description: 适用于V2ray的域名拦截规则集，每20分钟更新一次，确保即时同步上游减少误杀
 # Homepage: https://github.com/REIJI007/AdBlock_Rule_For_V2ray
-# LICENSE1：https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-GPL3.0
-# LICENSE2：https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-CC%20BY-NC-SA%204.0
+# LICENSE1: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-GPL3.0
+# LICENSE2: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-CC%20BY-NC-SA%204.0
 
 # 定义广告过滤器URL列表
 $urlList = @(
@@ -187,8 +187,8 @@ $textContent = @"
 # Title: AdBlock_Rule_For_V2ray
 # Description: 适用于V2ray的域名拦截列表，每20分钟更新一次，确保即时同步上游减少误杀
 # Homepage: https://github.com/REIJI007/AdBlock_Rule_For_V2ray
-# LICENSE1：https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-GPL3.0
-# LICENSE2：https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-CC%20BY-NC-SA%204.0
+# LICENSE1: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-GPL3.0
+# LICENSE2: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-CC%20BY-NC-SA%204.0
 # Generated on: $generationTime
 # Generated AdBlock rules
 # Total entries: $ruleCount
