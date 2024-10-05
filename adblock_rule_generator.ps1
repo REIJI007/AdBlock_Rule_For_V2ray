62499cb2dc16989c36b50946e1231deecd9c3a2c
REIJI007 <miyajimareji007@gmail.com>
2024-10-06 02:43:47 +0800

    Update adblock_rule_generator.ps1
    Signed-off-by: REIJI007 <miyajimareji007@gmail.com>

--- adblock_rule_generator.ps1
+++ adblock_rule_generator.ps1
@@ -537,10 +537,10 @@ $generationTime = (Get-Date).ToUniversalTime().AddHours(8).ToString("yyyy-MM-dd 
 # 创建文本格式的字符串
 $textContent = @"
 # Title: AdBlock_Rule_For_V2ray
-# Description: 适用于V2ray的域名拦截列表，每20分钟更新一次，确保即时同步上游减少误杀
+# Description: 适用于V2ray的域名拦截规则集，每20分钟更新一次，确保即时同步上游减少误杀
 # Homepage: https://github.com/REIJI007/AdBlock_Rule_For_V2ray
-# LICENSE1: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-GPL3.0
-# LICENSE2: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-CC%20BY-NC-SA%204.0
+# LICENSE1: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-GPL 3.0
+# LICENSE2: https://github.com/REIJI007/AdBlock_Rule_For_V2ray/blob/main/LICENSE-CC-BY-NC-SA 4.0
 # Generated on: $generationTime
 # Generated AdBlock rules
 # Total entries: $ruleCount
